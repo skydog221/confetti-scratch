eb53f0dc6577a2b750354db8ce4705f688728060
skydog221 <MoreBugOfDog@outlook.com>
2025-08-27 17:11:07 +0800

    feat:喷射、烟花、下雪、自定义功能

--- src/l10n/index.ts
+++ src/l10n/index.ts
@@ -1,3 +1,30 @@
 // Contains l10n
 
-export default {}
+export default {
+  "zh-cn": {
+    "confetti.name": "彩带喷射特效",
+    "confetti.fullConfetti": "💥以中心为原点喷射(撒花),特效 [ctype]",
+    "confetti.type.caidai": "🎉彩带",
+    "confetti.type.star": "⭐星星",
+    "confetti.sideConfetti":
+      "🎊从两侧喷射(庆祝),特效 [ctype],持续时间 [time] 秒",
+    "confetti.snowConfetti": "❄️下雪特效,持续时间 [time] 秒",
+    "confetti.label.1": "🔥预制特效",
+    "confetti.label.2": "🛠️高级自定义",
+    "confetti.custom":
+      "(留空即取默认值)在网页 x [x] (0~1),y [y] (0~1),方向 [d] (90为垂直向上),发射特效 [ctype].初速度 [s] ,颜色 [co] (HEX 字符串数组,带#)",
+  },
+  en: {
+    "confetti.name": "Confetti",
+    "confetti.fullConfetti": "💥Based on the center, effect [ctype]",
+    "confetti.type.caidai": "🎉Confetti",
+    "confetti.type.star": "⭐Star",
+    "confetti.sideConfetti":
+      "🎊From the sides , effect [ctype], duration [time] seconds (celebration)",
+    "confetti.snowConfetti": "❄️Snow effect, duration [time] seconds",
+    "confetti.label.1": "🔥Preset effects",
+    "confetti.label.2": "🛠️Advanced customization",
+    "confetti.custom":
+      "(Leave blank to use default value) In the web page, x [x] (0~1), y [y] (0~1), direction [d] (90 is vertical up), launch effect [ctype]. Initial speed [s], color [co] (HEX string array,with #)",
+  },
+};
